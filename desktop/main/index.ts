417df446034272dd8c7191412d16a19fd3f2d9d6
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-06-21 01:13:37 -0500

    Implement PatchedBrowserWindow in Electron
    The main change involved adding a new PatchedBrowserWindow class to the Electron Desktop application. This class extends Electron's BrowserWindow class and overrides its constructor to provide custom BrowserWindow creation options. Also, the code for an AppSettings global handler was added, which allows global devtools and update options to be set.

--- desktop/main/index.ts
+++ desktop/main/index.ts
@@ -1,1 +1,43 @@
 // # patch electron BrowserWindow and set up ipc
+// # done for you. lil cinamin.. cinamon? cinnamon! yeah. cinnamon roll
+
+import * as electron from "electron";
+import {BrowserWindow} from "electron";
+import {join} from "path";
+
+const {env} = process;
+
+export default class PatchedBrowserWindow extends BrowserWindow {
+    constructor(opts: Electron.BrowserWindowConstructorOptions) {
+        env.DISCORD_PRELOADER = opts.webPreferences!.preload;
+
+        opts.webPreferences!.preload = join(__dirname, "preload.min.js");
+
+        return new BrowserWindow(opts);
+
+        super();
+    }
+}
+
+// i no no tink dis works
+let appSettings: { settings?: any; };
+
+Object.defineProperty(global, "appSettings", {
+    get: () => appSettings,
+    set: (v: { settings?: any, }) => {
+        if (!v.hasOwnProperty("settings")) v.settings = {};
+        v.settings.DANGEROUS_ENABLE_DEVTOOLS_ONLY_ENABLE_IF_YOU_KNOW_WHAT_YOURE_DOING = true;
+        v.settings.SKIP_HOST_UDPATE = true // does this even work
+        appSettings = v;
+    }
+});
+
+const electronModule = require.resolve("electron");
+delete require.cache[electronModule]!.exports;
+
+const electronMod: typeof Electron.CrossProcessExports = {
+    ...electron,
+    BrowserWindow: PatchedBrowserWindow
+};
+
+require.cache[electronModule]!.exports = electronMod;
