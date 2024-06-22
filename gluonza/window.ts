aee54bfeb7418d6ba5e68f3e2131620e77ee37c7
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-06-22 05:20:45 -0500

    Clean up

--- gluonza/window.ts
+++ gluonza/window.ts
@@ -1,7 +1,6 @@
 import {api} from "./api/webpack/api";
 import {injectCSS, uninjectCSS} from "common/dom.js";
 import {Dashboard, OpenSettingsButton} from "./api/protons/settings/index..js";
-import {__jsx__} from "typings";
 
 export const gluanza = {
   webpack: api,
