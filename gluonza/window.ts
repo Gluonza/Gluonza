288fc4861694d729d14ce2d182a183f2688662ab
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-06-22 05:20:11 -0500

    Add CSS injection methods to gluonza
    The above changes include adding CSS injection and un-injection methods into the gluonza export in window.ts. This update supports more flexible styles handling within the application.

--- gluonza/window.ts
+++ gluonza/window.ts
@@ -1,8 +1,11 @@
 import {api} from "./api/webpack/api";
+import {injectCSS, uninjectCSS} from "common/dom.js";
 import {Dashboard, OpenSettingsButton} from "./api/protons/settings/index..js";
+import {__jsx__} from "typings";
 
 export const gluanza = {
   webpack: api,
+  css: {injectCSS, uninjectCSS},
   jsx: __jsx__,
     _self: {OpenSettingsButton, Dashboard},
   get React() { return api.common.React }
